76e1008eccd1b56fc53cdb6ac47c3afff66f5e12
kaizen-333 <kaizen3333333@gmail.com>
2025-05-15 00:50:06 +0400

    feat: launch app button styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,12 +39,12 @@ export default function Home() {
           <div className={styles.logoNav}>
             <img src="/img/OF_lockup_white.png" alt="Orion Finance Logo" className={styles.logo} />
           </div>
-          <ul className={styles.navLinks}>
-            <li><a href="/docs/introduction/orion" target="_blank">Docs</a></li>
-            <li><a href="https://discord.gg/8bAXxPSPdw" target="_blank">Discord</a></li>
-            <li><a href="dataroom.html" target="_blank">Data Room</a></li>
-            <li><a href="app.html" target="_blank">Launch App</a></li>
-          </ul>
+            <ul className={styles.navLinks}>
+              <li><a href="/docs/introduction/orion" target="_blank">Docs</a></li>
+              <li><a href="https://discord.gg/8bAXxPSPdw" target="_blank">Discord</a></li>
+              <li><a href="dataroom.html" target="_blank">Data Room</a></li>
+              <li><a href="app.html" target="_blank" className={styles.navDownloadButton}>Launch App</a></li>
+            </ul>
           <button
             className={styles.hamburger}
             aria-label="Open menu"
@@ -70,7 +70,7 @@ export default function Home() {
             <li><a href="/docs/introduction/orion" target="_blank" onClick={() => setIsMenuOpen(false)}>Docs</a></li>
             <li><a href="https://discord.gg/8bAXxPSPdw" target="_blank" onClick={() => setIsMenuOpen(false)}>Discord</a></li>
             <li><a href="dataroom.html" target="_blank" onClick={() => setIsMenuOpen(false)}>Data Room</a></li>
-            <li><a href="app.html" target="_blank" onClick={() => setIsMenuOpen(false)}>Launch App</a></li>
+            <li><a href="app.html" target="_blank" className={styles.navDownloadButton} onClick={() => setIsMenuOpen(false)}>Launch App</a></li>
           </ul>
         </div>
 
